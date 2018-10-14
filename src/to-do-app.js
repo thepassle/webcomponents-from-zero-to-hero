1bd1208d33b64983fb3dd2e1a9a9c7474b366c74
thepassle <pascalschilp@gmail.com>
2018-10-14 15:48:49 +0200

    Add source code

---
+++ src/to-do-app.js
@@ -0,0 +1,95 @@
+import './to-do-item.js';
+
+(() => {
+    const template = document.createElement('template');
+    template.innerHTML = `
+        <style>
+            :host {
+                display: block;
+                font-family: sans-serif;
+                text-align: center;
+            }
+
+            button {
+                border: none;
+                cursor: pointer;
+            }
+
+            ul {
+                list-style: none;
+                padding: 0;
+            }
+        </style>
+        <h1>To do</h1>
+
+        <input type="text" placeholder="Add a new to do"></input>
+        <button>✅</button>
+
+        <ul id="todos"></ul>
+    `;
+
+    class TodoApp extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
+        }
+
+        connectedCallback() {
+            this.$todoList = this._shadowRoot.querySelector('ul');
+
+            this.$input = this._shadowRoot.querySelector('input');
+            this.$submitButton = this._shadowRoot.querySelector('button');
+            this.$submitButton.addEventListener('click', this._addTodo.bind(this));
+        }
+
+        _removeTodo(e) {
+            this._todos.splice(e.detail, 1);
+            this._renderTodoList();
+        }
+
+        _toggleTodo(e) {
+            const item = this._todos[e.detail];
+            this._todos[e.detail] = Object.assign({}, item, {
+                checked: !item.checked
+            });
+            this._renderTodoList();
+        }
+
+        _addTodo() {
+            this._todos.push({ text: this.$input.value, checked: false })
+            this._renderTodoList();
+            this.$input.value = '';
+        }
+
+        _renderTodoList() {
+            this.$todoList.innerHTML = '';
+
+            this._todos.forEach((item, index) => {
+                let $item = document.createElement('to-do-item');
+                $item.setAttribute('text', item.text);
+
+                if(item.checked) {
+                    $item.setAttribute('checked', '');                
+                }
+
+                $item.index = index;
+                $item.addEventListener('onRemove', this._removeTodo.bind(this));
+                $item.addEventListener('onToggle', this._toggleTodo.bind(this));
+
+                this.$todoList.appendChild($item);
+            });
+        }
+
+        set todos(value) {
+            this._todos = value;
+            this._renderTodoList();
+        }
+
+        get todos() {
+            return this._todos;
+        }
+    }
+
+    window.customElements.define('to-do-app', TodoApp);
+})();
