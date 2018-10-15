4090c9714b8d7180fb3e1300c10e5bb24527fa7d
thepassle <pascalschilp@gmail.com>
2018-10-15 20:13:23 +0200

    Rework props/attrs, add todos

--- src/to-do-item.js
+++ src/to-do-item.js
@@ -1,103 +1,102 @@
-(() => {
-    const template = document.createElement('template');
-    template.innerHTML = `
-        <style>
-            :host {
-                display: block;
-                font-family: sans-serif;
-            }
 
-            .completed {
-                text-decoration: line-through;
-            }
-
-            button {
-                border: none;
-                cursor: pointer;
-            }
-        </style>
-        <li class="item">
-            <input type="checkbox">
-            <label></label>
-            <button>❌</button>
-        </li>
-    `;
-
-    class TodoItem extends HTMLElement {
-        constructor() {
-            super();
-            this._shadowRoot = this.attachShadow({ 'mode': 'open' });
-            this._shadowRoot.appendChild(template.content.cloneNode(true));
-
-            this._text = '';
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        :host {
+            display: block;
+            font-family: sans-serif;
         }
 
-        connectedCallback() {
-            this.$item = this._shadowRoot.querySelector('.item');
-            this.$removeButton = this._shadowRoot.querySelector('button');
-            this.$text = this._shadowRoot.querySelector('label');
-            this.$checkbox = this._shadowRoot.querySelector('input');
-
-            this.$removeButton.addEventListener('click', (e) => {
-                this.dispatchEvent(new CustomEvent('onRemove', { detail: this.index }));
-            });
-
-            this.$checkbox.addEventListener('click', (e) => {
-                this.dispatchEvent(new CustomEvent('onToggle', { detail: this.index }));
-            });
-
-            this._renderTodo();
+        .completed {
+            text-decoration: line-through;
         }
 
-        static get observedAttributes() {
-            return ['text', 'checked', 'index'];
+        button {
+            border: none;
+            cursor: pointer;
         }
+    </style>
+    <li class="item">
+        <input type="checkbox">
+        <label></label>
+        <button>❌</button>
+    </li>
+`;
 
-        attributeChangedCallback(name, oldValue, newValue) {
-            switch(name){
-                case 'text':
-                    this._text = newValue;
-                    break;
-                case 'checked':
-                    this._checked = this.hasAttribute('checked');
-                    break;
-                case 'index':
-                    this._index = parseInt(newValue);
-                    break;
-            }
-        }
+class TodoItem extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-        set index(val) {
-            this.setAttribute('index', val);
-        }
-
-        get index() {
-            return this._index;
-        }
-
-        get checked() {
-            return this.hasAttribute('checked');
-        }
-
-        set checked(val) {
-            if (val) {
-                this.setAttribute('checked', '');
-            } else {
-                this.removeAttribute('checked');
-            }
-        }
-
-        _renderTodo() {
-            this.$text.innerHTML = this._text;
-            if (this._checked) {
-                this.$item.classList.add('completed');
-                this.$checkbox.setAttribute('checked', '');
-            } else {
-                this.$item.classList.remove('completed');
-                this.$checkbox.removeAttribute('checked');
-            }
-        }
-
+        this._text = '';
     }
-    window.customElements.define('to-do-item', TodoItem);
-})();
+
+    connectedCallback() {
+        this.$item = this._shadowRoot.querySelector('.item');
+        this.$removeButton = this._shadowRoot.querySelector('button');
+        this.$text = this._shadowRoot.querySelector('label');
+        this.$checkbox = this._shadowRoot.querySelector('input');
+
+        this.$removeButton.addEventListener('click', (e) => {
+            this.dispatchEvent(new CustomEvent('onRemove', { detail: this.index }));
+        });
+
+        this.$checkbox.addEventListener('click', (e) => {
+            this.dispatchEvent(new CustomEvent('onToggle', { detail: this.index }));
+        });
+
+        this._renderTodo();
+    }
+
+    static get observedAttributes() {
+        return ['text', 'checked', 'index'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch(name){
+            case 'text':
+                this._text = newValue;
+                break;
+            case 'checked':
+                this._checked = this.hasAttribute('checked');
+                break;
+            case 'index':
+                this._index = parseInt(newValue);
+                break;
+        }
+    }
+
+    set index(val) {
+        this.setAttribute('index', val);
+    }
+
+    get index() {
+        return this._index;
+    }
+
+    get checked() {
+        return this.hasAttribute('checked');
+    }
+
+    set checked(val) {
+        if (val) {
+            this.setAttribute('checked', '');
+        } else {
+            this.removeAttribute('checked');
+        }
+    }
+
+    _renderTodo() {
+        this.$text.innerHTML = this._text;
+        if (this._checked) {
+            this.$item.classList.add('completed');
+            this.$checkbox.setAttribute('checked', '');
+        } else {
+            this.$item.classList.remove('completed');
+            this.$checkbox.removeAttribute('checked');
+        }
+    }
+
+}
+window.customElements.define('to-do-item', TodoItem);
